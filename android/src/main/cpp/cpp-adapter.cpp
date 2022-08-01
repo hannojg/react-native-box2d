96faf1f7c8f105197621d7e32ba58860f843e765
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-08-01 08:53:51 +0200

    remove unused cpp template code

--- android/src/main/cpp/cpp-adapter.cpp
+++ android/src/main/cpp/cpp-adapter.cpp
@@ -1,15 +1,10 @@
 #include <jni.h>
 #include <jsi/jsi.h>
-#include "FastCryptoHostObject.h"
 #include "JSIBox2dApi.h"
 
 using namespace facebook;
 
 void install(jsi::Runtime& runtime) {
-    auto hostObject = std::make_shared<margelo::FastCryptoHostObject>();
-    auto object = jsi::Object::createFromHostObject(runtime, hostObject);
-    runtime.global().setProperty(runtime, "__FastCryptoProxy", std::move(object));
-
     // add box2d api
     auto box2dApi = std::make_shared<Box2d::JSIBox2dApi>(runtime);
     auto box2dApiHostObject = jsi::Object::createFromHostObject(runtime, box2dApi);
@@ -23,5 +18,5 @@ Java_com_reactnativefastcrypto_FastCryptoModule_nativeInstall(JNIEnv *env, jobje
     if (runtime) {
         install(*runtime);
     }
-    // if runtime was nullptr, FastCrypto will not be installed. This should only happen while Remote Debugging (Chrome), but will be weird either way.
+    // if runtime was nullptr, Box2d will not be installed. This should only happen while Remote Debugging (Chrome), but will be weird either way.
 }
