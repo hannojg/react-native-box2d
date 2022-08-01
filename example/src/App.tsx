74d27410c9a96e42f650cc091a80fecf21f5f09a
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-08-01 21:03:10 +0200

    fix example + useClock from Skia

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { Box2d } from 'react-native-fast-crypto';
+import { useClockValue } from '@shopify/react-native-skia';
 
 export default function App() {
   const [v, setV] = React.useState<number>();
+  const clock = useClockValue();
 
   // Reproduction from: https://box2d.org/documentation/md__d_1__git_hub_box2d_docs_hello.html
   React.useEffect(() => {
@@ -42,17 +44,29 @@ export default function App() {
     const velocityIterations = 6;
     const positionIterations = 2;
 
-    for (let i = 0; i < 60; i++) {
+    const remove = clock.addListener(() => {
       world.Step(timeStep, velocityIterations, positionIterations);
       const position = body.GetPosition();
       const rotation = body.GetAngle();
       console.log(
-        `#${i} - x: ${position.x.toFixed(2)}, y: ${position.y.toFixed(
+        `x: ${position.x.toFixed(2)}, y: ${position.y.toFixed(
           2
         )}, rotation: ${rotation.toFixed(2)}`
       );
-    }
-  }, [v]);
+    });
+    clock.start();
+    return remove;
+    // for (let i = 0; i < 60; i++) {
+    //   world.Step(timeStep, velocityIterations, positionIterations);
+    //   const position = body.GetPosition();
+    //   const rotation = body.GetAngle();
+    //   console.log(
+    //     `#${i} - x: ${position.x.toFixed(2)}, y: ${position.y.toFixed(
+    //       2
+    //     )}, rotation: ${rotation.toFixed(2)}`
+    //   );
+    // }
+  }, [clock, v]);
 
   return (
     <View style={styles.container}>
